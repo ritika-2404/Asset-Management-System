4c23a5a650c0298c66edc5000935ac6b14591151
ritika-2404 <r29473185@gmail.com>
2025-10-27 10:10:10 +0530

    Delete app.py

--- app.py
+++
@@ -1,8 +0,0 @@
-from flask import Flask
-from routes import asset_routes
-
-app = Flask(__name__)
-app.register_blueprint(asset_routes.asset_bp, url_prefix='/assets')
-
-if __name__ == "__main__":
-    app.run(debug=True)
